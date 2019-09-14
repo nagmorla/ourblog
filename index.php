5ea5a99ac462dd32e293b61e2e77ea0f733ca126
nagmorla <nagmorla@users.noreply.github.com>
2019-09-14 23:44:05 +0530

    Code changes ...

--- index.php
+++ index.php
@@ -29,36 +29,16 @@ and open the template in the editor.
         <script src='https://www.google.com/recaptcha/api.js'></script>
     </head>
     <body>
-        <!--        <nav class="navbar navbar-inverse navbar-fixed-top">
-                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul class="nav navbar-nav navbar-right">
-                            <li class="active">
-                                <a class="page-scroll" href="#home_div">Home</a>
-                            </li>
-                            <li>
-                                <a class="page-scroll" href="#about_div">About</a>
-                            </li>                        
-                            <li>
-                                <a class="page-scroll" href="#careers_div">Careers</a>
-                            </li>
-                            <li>
-                                <a class="page-scroll" href="#contact_div">Contact</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>-->
-
-
         <nav class="navbar navbar-inverse navbar-fixed-top">
             <div class="container-fluid">
                 <div class="navbar-header">
                     <a class="navbar-brand" href="#">My Company Logo and Name</a>
                 </div>
-                <ul class="nav navbar-nav">
-                    <li class="active"><a data-toggle="tab" href="#home_div">Home</a></li>
-
-                    <li><a data-toggle="tab" href="#about_div">About Us</a></li>
-                </ul>
+                <!--                <ul class="nav navbar-nav">
+                                    <li class="active"><a data-toggle="tab" href="#home_div">Home</a></li>
+                
+                                    <li><a data-toggle="tab" href="#about_div">About Us</a></li>
+                                </ul>-->
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
                     <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
@@ -70,20 +50,97 @@ and open the template in the editor.
             <div>
                 Some info ...... like company agenda.
             </div>
-            <div id="Home_Screen">
-                <div id="home_div" style="height: 300px; margin-top: 50px">
-                    
-                    <div data-bind="foreach: {data: topics, as: 'topic'}" class="topics_cont">
-                        <div style="" class="_topic">
-                            <a href="#" data-bind="click: $parent.gotoTopic"><span data-bind="text: subject"> </span></a><BR>
-                            <span data-bind="text: content"> </span>
+            <div id="Home_Screen" style="margin-top: 50px !important;">
+                <div class="row">
+                    <div class="col-md-9">
+                        <div data-bind="foreach: {data: topics, as: 'topic'}" class="topics_cont">
+                            <div style="" class="_topic">
+                                <div class="col-md-2 col-xs-3 p-0">
+                                    <div class="blog-date"> 
+                                        <span class="bdate" data-bind="html: t_day+'<BR>'+t_mon"></span>
+                                        <span class="byear" data-bind="html: t_year"></span> 
+                                    </div>
+                                </div>
+                                <div class="col-md-10">
+                                    <a href="#" data-bind="click: $parent.gotoTopic"><span data-bind="text: subject"> </span></a><BR>
+                                    <span data-bind="text: content"> </span><BR>
+                                    <div class="pull-right"> <a href="#" data-bind="click: $parent.gotoTopic">READ MORE...</a> </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div id="about_div" style="height: 300px">Hi, This is About Div</div>
-                <div id="careers_div" style="height: 300px">Hi, This is Career Div</div>
-                <div id="contact_div" style="height: 300px">Hi, This is Contact Div</div>
+                    <div class="col-md-3">
+                        <div class="side-bar">
+                            <h4 class="sidebar-heading m-b-30 m-l-10 m-r-10">Categories</h4>
+                            <ul class="cat-list">
+                                <li class="m-b-10"><a href="https://mindmajix.com/denodo"><i class="fa fa-caret-right"></i>Data Science</a></li>
+                                <li class="m-b-10"><a href="https://mindmajix.com/mongodb"><i class="fa fa-caret-right"></i>Machine Learning</a></li>
+                                <li class="m-b-10"><a href="https://mindmajix.com/oracle-financials"><i class="fa fa-caret-right"></i>Artificial Intelligence</a></li>
+                                <li class="m-b-10"><a href="https://mindmajix.com/oracle-odi"><i class="fa fa-caret-right"></i>Grafana</a></li>
+                                <li class="m-b-10"><a href="https://mindmajix.com/checkpoint"><i class="fa fa-caret-right"></i>Tabview</a></li>
+                            </ul>
+                            <a href="https://mindmajix.com/blog-category" class="btn-60 btn bg-red f-s-14 m-t-50 m-b-20">VIEW MORE</a>
+                        </div>
 
+                        <div class="form-wrapper m-t-20" id="form_sticky" style="opacity: 4.276;">
+                            <h4 class="sidebar-heading-center f-s-18 m-b-20">Drop us a Query</h4>
+                            <div class="p-r-10 p-l-10">
+                                <form action="https://mindmajix.com/form_insert" method="POST">
+
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <label for="fullname" class="floating-label">Full Name</label>
+                                            <input type="text" required="" name="fullname"> 
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <label for="email" class="floating-label">E-mail Address</label>
+                                            <input type="email" name="email" required=""> 
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <label for="course-name" class="floating-label">Course Name</label>
+                                            <input type="text" name="course_name" required=""> 
+                                        </div>
+                                    </div>
+
+                                    <span id="ierror" class="color-theme"></span>
+                                    <div class="row">
+                                        <div class="col-md-3 col-xs-3 p-r-0 country_code m-t-7">
+                                            <select id="drop_cc" name="c_code" class="form-control">
+                                                <option value="US" data-code="+1" data-text="United States">US</option>
+                                                <option value="MZ" data-code="+258" data-text="Mozambique">Mozambique</option>
+                                            </select>
+                                        </div>
+                                        <div class="col-md-2 col-xs-2 p-l-0 p-r-0">
+                                            <label class="p-l-4 floating-label"></label>
+                                            <input type="tel" id="drop_cval" name="phone_code" readonly="" value="" required=""> 
+                                        </div>
+                                        <div class="col-md-7 col-xs-7 p-l-0">
+                                            <label class="p-l-4 floating-label">Phone Number *</label>
+                                            <input type="tel" name="phone_number" onkeypress="return isNumberKey(event)" maxlength="15" required=""> 
+                                        </div>
+                                    </div>                    
+
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <label for="message" class="floating-label">Message</label>
+                                            <textarea rows="2" name="message" maxlength="200"></textarea>
+                                        </div>
+                                    </div>
+
+                                    <button type="submit" class="btn-60 btn bg-red waves-effect waves-light f-s-14">Contact Us</button>
+                                </form>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
             </div>
             <div id="topic_details" style="margin-top: 3%;">
                 <div style="text-align: right;"><a href="#" onclick="gotoMain();">Goto Home</a></div>
--- js/komain.js
+++ js/komain.js
@@ -1,10 +1,20 @@
 function HomeModel() {
     var self = this;
     self.topics = ko.observableArray([]);
-    var topic = {'subject': 'Artificial Inteligence :: Robot is very dangorous', 'content': 'A robot is a machine—especially one programmable by a computer— capable of carrying out a complex series of actions automatically. '};
+    var topic = {'subject': 'Artificial Inteligence :: Robot is very dangorous',
+        'content': 'A robot is a machine—especially one programmable by a computer— capable of carrying out a complex series of actions automatically. ',
+        't_day': '12',
+        't_mon': 'Sep',
+        't_year': '2019'
+    };
     self.topics.push(topic);
 
-    topic = {'subject': 'Machine Learning:: Linear regression is difficult to understand', 'content': 'Make better decisions utilizing linear regression techniques. Learn more today. Leverage IBM® SPSS for simple linear regression. '};
+    topic = {'subject': 'Machine Learning:: Linear regression is difficult to understand',
+        'content': 'Make better decisions utilizing linear regression techniques. Learn more today. Leverage IBM® SPSS for simple linear regression. ',
+        't_day': '13',
+        't_mon': 'Sep',
+        't_year': '2019'
+    };
     self.topics.push(topic);
 
     self.gotoTopic = function (data) {
